9afa6f1da8c0bd27e11aff03a6ace244b3c2d9a9
rodaan <rodaan.rabang@gmail.com>
2017-03-11 22:46:56 -0800

    Completed JIMP conversion function and saving the S3

--- handler.js
+++ handler.js
@@ -1,27 +1,58 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+const Jimp = require('jimp');
+const fetch = require('node-fetch');
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+const IMGBUCKET = process.env.IMGBUCKET;
+const JIMPBUCKET = process.env.JIMPBUCKET;
 
-  callback(null, response);
+module.exports.createAndSaveJimp = (event, context, callback) => {
+    console.log(event);
+    const imageURL = event.image_url || JSON.parse(event.body).image_url;
+    const key = event.key || JSON.parse(event.body).key;
+    // Download the image from s3 into a buffer
+    s3.getObject({
+        Bucket: IMGBUCKET,
+        Key: key
+    }, (err, data) => {
+        if(err){
+            console.log(err, err.stack);
+        }
+        console.log(data);
+        // Convert image to JIMP
+        Jimp.read(data, function(err, image){
+            if(err){
+                console.log(err);
+            }
+            // Put JIMP in s3 
+            s3.putObject({
+                Bucket: JIMPBUCKET,
+                Key: "images/" + _sizesArray[
+                        index].destinationPath +
+                    "/" + fileName.slice(0, -4) +
+                    ".jpg",
+                Body: data,
+                ContentType: 'JPG'
+            }, (err, data) => {
+                if(err){
+                    console.log(err, err.stack);
+                }
+                // TODO: Put JIMP info in Firebase database
+                console.log(data);
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+            });
+        });
+    });
 };
 
-module.exports.shit = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-        message: 'shit',
-        input: event,
-    }),
-  };
-  callback(null, response);
-};
+module.exports.test = (event, context, callback) => {
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: "running",
+            input: {}
+        })
+    }
+    callback(null, response);
+}
